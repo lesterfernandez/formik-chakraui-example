a401fe3329e6bdaea2c4680705d6fd11fa8d2e6c
Lester Fernandez <5958256+lesterfernandez@users.noreply.github.com>
2021-11-22 08:38:29 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import { Button } from "@chakra-ui/button";
+import { Heading, VStack } from "@chakra-ui/layout";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import TextField from "./TextField";
+
 function App() {
-  return <></>;
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required("Username required")
+          .min(6, "Username is too short"),
+        password: Yup.string()
+          .required("Password required")
+          .min(6, "Password is too short"),
+        email: Yup.string().email("invalid email").required("email required"),
+      })}
+      onSubmit={(values, actions) => {
+        alert(JSON.stringify(values, null, 2));
+        actions.resetForm();
+      }}
+    >
+      {formik => (
+        <VStack
+          as="form"
+          mx="auto"
+          w={{ base: "90%", md: 500 }}
+          h="100vh"
+          justifyContent="center"
+          onSubmit={formik.handleSubmit}
+        >
+          <Heading>Sign Up</Heading>
+
+          <TextField name="username" placeholder="enter username" />
+          <TextField name="email" placeholder="enter email" type="email" />
+
+          <TextField
+            name="password"
+            type="password"
+            placeholder="enter password"
+          />
+
+          <Button type="submit" variant="outline" colorScheme="teal">
+            Create Acc
+          </Button>
+        </VStack>
+      )}
+    </Formik>
+  );
 }
 
 export default App;
---
+++ src/TextField.jsx
@@ -0,0 +1,20 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+} from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { Field, useField } from "formik";
+
+const TextField = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <FormControl isInvalid={meta.error && meta.touched}>
+      <FormLabel>{label}</FormLabel>
+      <Field as={Input} {...field} {...props} />
+      <FormErrorMessage>{meta.error}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default TextField;
